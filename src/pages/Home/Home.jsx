21eee201108f013506dbdf820bb453063e01f9a3
anasyakubu <yakubuanas04@gmail.com>
2024-11-12 16:47:02 +0100

    added nav and folder structure

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,60 @@
+// import React from "react";
+import Hero from "../Home/Home";
+
+const Home = () => {
+  return (
+    <div className="Home">
+      {/* <Hero /> */}
+      <h6>Home</h6>
+      {/* <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6> */}
+      {/* <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6>
+      <h6>Home</h6> */}
+    </div>
+  );
+};
+
+export default Home;
