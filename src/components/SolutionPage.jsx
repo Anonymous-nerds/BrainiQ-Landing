f730e83babfcd078ee75f25ae8923893fbf2d506
Beebarh.jy <153818039+Barbexa@users.noreply.github.com>
2024-11-19 14:53:47 +0100

    Lets try again

--- src/components/SolutionPage.jsx
+++ src/components/SolutionPage.jsx
@@ -2,7 +2,7 @@ const SolutionPage = () => {
   return (
     <div className="relative mt-20 border-b border-neutral-800 min-h-[800px]">
         <div className="text-center">
-            <span className="bg-neutral-900 text-orange-500 rounded-full h-6"></span>
+            <span className="bg-neutral-200 text-blue-500 rounded-md h-6 text-sm font-medium px-3 py-2 uppercase">Our Solution</span>
         </div>
     </div>
   )
