18e4a17df20de343e488d45f5c094c211255638e
miss-naseer <167442258+miss-naseer@users.noreply.github.com>
2024-11-25 11:46:12 +0100

    about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React from 'react'
+import Nav from '../../components/Nav'
 
 const About = () => {
-  return <div className="About">About</div>;
-};
+  return (
+  <div>
+    <div>
+    <Nav/>
+    <div className='mt-8 mb-9'></div>
+    </div>
+    <div className='flex flex-col items-center mt-6 lg:mt-20'>
+      <h2 className="text-5xl sm:text-6xl lg:text-6xl text-center tracking-wide">About</h2>
+    </div>
+  </div>
+  
 
-export default About;
+
+
+  )
+}
+
+export default About
